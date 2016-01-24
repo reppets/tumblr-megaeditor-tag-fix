7771dc38711e5e4b5688c188609f7f47af08788f
reppets <all.you.need.is.word@gmail.com>
2016-01-24 22:30:46 +0900

    add the userscript.

---
+++ tumblr-megaeditor-tag-fix.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name tumblr-megaeditor-tag-fix
+// @description unescapes non-ascii chars escaped on tumblr mega-editor's tag edit tab.
+// @description:ja Tumblrの複数投稿編集ツール(mega-editor)での日本語タグなどの非Asciiタグが文字化け(エスケープ: %uXXXX形式に変換)されてしまうのを修正します。
+// @namespace http://reppets.hatenablog.com/
+// @version 1.0.0
+// @compatible firefox (verified with 43.0.4)
+
+// @include https://www.tumblr.com/mega-editor/*
+// ==/UserScript==
+var observer = new MutationObserver(function(records, observer) {
+	var nodeList = document.querySelectorAll('#tags label');
+	var nodes = Array.prototype.slice.call(nodeList,0);  // to use forEach() method.
+	nodes.forEach(function(elem, index, array){
+		elem.textContent = unescape(elem.textContent);
+	});
+});
+
+observer.observe(document.getElementById('tags'), {childList: true});
